02312e0914eef2cd18b9abd8eecc398ff704bfa2
Andrei Ryzhykau <andrei.ryzhykau@outstaff.exness.com>
2024-01-05 02:44:28 +0100

    update policies.tf

--- policies.tf
+++ policies.tf
@@ -10,7 +10,7 @@ data "aws_iam_policy_document" "s3" {
       "glacier:List"
     ]
     resources = ["arn:aws:s3:::*"]
-    effect = "Allow"
+    effect    = "Allow"
   }
 }
 
@@ -21,7 +21,7 @@ data "aws_iam_policy_document" "rds" {
       "rds:Describe*"
     ]
     resources = ["arn:aws:rds:*"]
-    effect = "Allow"
+    effect    = "Allow"
   }
 }
 
@@ -37,98 +37,152 @@ data "aws_iam_policy_document" "ecs" {
       "ecr:Get*",
     ]
     resources = ["arn:aws:rds:*"]
-    effect = "Allow"
+    effect    = "Allow"
   }
 }
 
-data "aws_iam_policy_document" "iam" {  statement {
+data "aws_iam_policy_document" "iam" {
+  statement {
     sid = "RDSReadAccess"
     actions = [
       "iam:Get*",
-"iam:List*"
+      "iam:List*"
     ]
     resources = ["arn:aws:iam:*"]
-    effect = "Allow"
-  }}
+    effect    = "Allow"
+  }
+}
 
-data "aws_iam_policy_document" "vpc" {  statement {
+data "aws_iam_policy_document" "vpc" {
+  statement {
     sid = "RDSReadAccess"
-    actions = [ "ec2:Get",
-"ec2:List",
+    actions = [
+      "ec2:Get",
+      "ec2:List",
       "ec2:DescribeAvailbilityZones",
-"ec2:DescribeAdressTransfers",
-"ec2:DescribeAdrdreses",
-"ec2:DescribeAddressesAttribute",
-"ec2:DescribeAwsNetworkPerformanceMetricSubscriptions",
-"ec2:DescribeClientVpnAuthorizationRules",
-"ec2:DescribeClientVpnConnections",
-"ec2:DescribeClientVpnEndpoints",
-"ec2:DescribeClientVpnRoutes",
-"ec2:DescribeClientVpnTargetNetworks",
-"ec2:DescribeCoipPools",
-"ec2:DescribeCustomerGateways",
-"ec2:DescribeDhcpOptions",
-"ec2:DescribeEgressOnlylnternetGateways",
-"ec2:DescribeFlowLogs",
-"ec2:DescribelnternetGateways",
-"ec2:Describelpv6Pools",
-"ec2:DescribeLocalGatewayRouteTablePermissions",
-"ec2:DescribelLocalGatewayRouteTableVirtualInterfaceGroupAssociations",
-"ec2:DescrbeLocalGatewayRouteTableVpcAssociations",
-"ec2:DescribeLocalGatewayRouteTables",
-"ec2:DescribelLocalGatewayVirtualInterfaceGroups",
-"ec2:DescribelLocalGatewayVirtualInterfaces",
-"ec2:DescribeLocalGateways",
+      "ec2:DescribeAdressTransfers",
+      "ec2:DescribeAdrdreses",
+      "ec2:DescribeAddressesAttribute",
+      "ec2:DescribeAwsNetworkPerformanceMetricSubscriptions",
+      "ec2:DescribeClientVpnAuthorizationRules",
+      "ec2:DescribeClientVpnConnections",
+      "ec2:DescribeClientVpnEndpoints",
+      "ec2:DescribeClientVpnRoutes",
+      "ec2:DescribeClientVpnTargetNetworks",
+      "ec2:DescribeCoipPools",
+      "ec2:DescribeCustomerGateways",
+      "ec2:DescribeDhcpOptions",
+      "ec2:DescribeEgressOnlylnternetGateways",
+      "ec2:DescribeFlowLogs",
+      "ec2:DescribelnternetGateways",
+      "ec2:Describelpv6Pools",
+      "ec2:DescribeLocalGatewayRouteTablePermissions",
+      "ec2:DescribelLocalGatewayRouteTableVirtualInterfaceGroupAssociations",
+      "ec2:DescrbeLocalGatewayRouteTableVpcAssociations",
+      "ec2:DescribeLocalGatewayRouteTables",
+      "ec2:DescribelLocalGatewayVirtualInterfaceGroups",
+      "ec2:DescribelLocalGatewayVirtualInterfaces",
+      "ec2:DescribeLocalGateways",
 
 
     ]
     resources = ["arn:aws:ec2:*"]
-    effect = "Allow"
-  }}
+    effect    = "Allow"
+  }
+}
 
-data "aws_iam_policy_document" "cloudwatch" {}
+data "aws_iam_policy_document" "cloudwatch" {
+  statement {
+    sid = "RDSReadAccess"
+    actions = [
+      "cloudwatch:Get*",
+      "cloudwatch:List*",
+      "cloudwatch:Decsribe*"
+    ]
+    resources = ["arn:aws:cloudwatch:*"]
+    effect    = "Allow"
+  }
+}
 
-data "aws_iam_policy_document" "cloudfront" {}
+data "aws_iam_policy_document" "cloudfront" {
+  statement {
+    sid = "RDSReadAccess"
+    actions = [
+      "cloudfront:Get*",
+      "cloudfront:List*",
+      "cloudfront:Decsribe*"
+    ]
+    resources = ["arn:aws:cloudfront:*"]
+    effect    = "Allow"
+  }
+}
 
 data "aws_iam_policy_document" "apprunner" {}
 
-data "aws_iam_policy_document" "lambda" {}
+data "aws_iam_policy_document" "lambda" {
+  statement {
+    sid = "RDSReadAccess"
+    actions = [
+      "lambda:Get*",
+      "lambda:List*"
+    ]
+    resources = ["arn:aws:lambda:*"]
+    effect    = "Allow"
+  }
+}
 
-data "aws_iam_policy_document" "dynamodb" {}
+data "aws_iam_policy_document" "dynamodb" {
+  statement {
+    sid = "RDSReadAccess"
+    actions = [
+      "dynamodb:Get*",
+      "dynamodb:List*",
+      "dynamodb: Describe*"
+    ]
+    resources = ["arn:aws:dynamodb:*"]
+    effect    = "Allow"
+  }
+}
 
 data "aws_iam_policy_document" "cloudformation" {}
 
 data "aws_iam_policy_document" "apigateway" {}
 
-data "aws_iam_policy_document" "sqs" {  statement {
+data "aws_iam_policy_document" "sqs" {
+  statement {
     sid = "RDSReadAccess"
     actions = [
       "sqs:Get*",
-"sqs:List*
+      "sqs:List*"
     ]
     resources = ["arn:aws:sqs:*"]
-    effect = "Allow"
-  }}
+    effect    = "Allow"
+  }
+}
 
-data "aws_iam_policy_document" "sns" {  statement {
+data "aws_iam_policy_document" "sns" {
+  statement {
     sid = "RDSReadAccess"
     actions = [
-      "sns:Get*", 
-"sns:List*
+      "sns:Get*",
+      "sns:List*"
     ]
     resources = ["arn:aws:sns:*"]
-    effect = "Allow"
-  }}
+    effect    = "Allow"
+  }
+}
 
-data "aws_iam_policy_document" "ses" {  statement {
+data "aws_iam_policy_document" "ses" {
+  statement {
     sid = "RDSReadAccess"
     actions = [
-      "ses:Describe*", 
-"ses:Get*",
-"ses:List*"
+      "ses:Describe*",
+      "ses:Get*",
+      "ses:List*"
     ]
     resources = ["arn:aws:ses:*"]
-    effect = "Allow"
-  }}
+    effect    = "Allow"
+  }
+}
 
 data "aws_iam_policy_document" "backup" {}
